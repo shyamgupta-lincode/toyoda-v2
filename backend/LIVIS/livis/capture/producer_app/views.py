b135d7934bb2857d302337f7a2cb6e5dcaa50a06
maggienaik <meghana.naik@lincode.us>
2021-01-19 10:59:59 +0530

    sprint1 adding producer app

---
+++ backend/LIVIS/livis/capture/producer_app/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import TemplateHTMLRenderer
+from django.http import HttpResponse
+import json
+from .utils import *
+
+
+@api_view(['POST'])
+@renderer_classes((TemplateHTMLRenderer,))
+@csrf_exempt
+def bulk_upload(data):
+    data = json.loads(data.body)
+    message, status_code = start_bulk_upload_stream(data)
+    if status_code == 200:
+        return HttpResponse(json.dumps({'Message': 'Success!', 'data': message}), content_type="application/json")
+    else:
+        return HttpResponse(json.dumps({'Message': 'fail!', 'data': message}), content_type="application/json")
+
+
+@api_view(['POST'])
+@renderer_classes((TemplateHTMLRenderer,))
+@csrf_exempt
+def video_input(data):
+    data = json.loads(data.body)
+    message, status_code = start_video_stream(data)
+    if status_code == 200:
+        return HttpResponse(json.dumps({'Message': 'Success!', 'data': message}), content_type="application/json")
+    else:
+        return HttpResponse(json.dumps({'Message': 'fail!', 'data': message}), content_type="application/json")
