c9d7283c906be1f013d753afb413dc1e19224de7
Lincode <lincode@pop-os.localdomain>
2021-02-17 18:34:05 +0530

    deleting Capture outside backend

--- Capture/consumer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-
-from . import views
-
-urlpatterns = [
-    re_path(r'^bulk_upload/$', views.bulk_upload),
-    re_path(r'^video_input/$', views.video_input)
-]
