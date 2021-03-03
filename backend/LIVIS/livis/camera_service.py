e06b1b2ec0c0e4d5f2d80cfd607028684e116648
Lincode <lincode@pop-os.localdomain>
2021-03-03 17:17:46 +0530

    ip and dashboards testing in progress

--- backend/LIVIS/livis/camera_service.py
+++ backend/LIVIS/livis/camera_service.py
@@ -1,17 +1,18 @@
-import sys
+import urllib
 import cv2
-#import neoapi
-import time
-import datetime
+import numpy as np
+import urllib.request
+import matplotlib.pyplot as plt
 import os
 import time
+import tensorflow as tf
+import sys
+import datetime
 import base64
-import cv2
 from kafka import KafkaProducer
 import json
 import logging
 import pymongo
-import sys
 from common.utils import MongoHelper
 from livis.settings import *
 from kafka import KafkaProducer
@@ -19,16 +20,42 @@ import multiprocessing
 import threading
 from PIL import Image, ImageEnhance
 from matplotlib import cm
-import numpy as np
 import numpy
+
+
+class IPWEBCAM(object):
+    def __init__(self, root_url='10.60.60.254:8080', width=400, height=400):
+        self.url = 'http://'+root_url
+        self.width = width
+        self.height = height
+        self.resolutions = {
+                "0" : "1920x1080",
+                "1" : "1280x720",
+                "2" : "960x720",
+                "3" : "720x480",
+                "4" : "640x480",
+                "5" : "352x288",
+                "6" : "320x240",
+                "7" : "256x144",
+                "8" : "176x144"
+                }
+
+    def get_image(self):
+        # Get our image from the phone
+        imgResp = urllib.request.urlopen(self.url + '/shot.jpg')
+        # Convert our image to a numpy array so that we can work with it
+        imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+        # Convert our image again but this time to opencv format
+        img = cv2.imdecode(imgNp,-1)
+        return img
 
 class Camera():
     def __init__(self, KAFKA_BROKER_URL, topic, camera_id):
         self.tfms = {}
-        self.KAFKA_BROKER_URL =  KAFKA_BROKER_URL
+        self.KAFKA_BROKER_URL = KAFKA_BROKER_URL
         self.topic = topic
         self.cam_id = camera_id
-        self.cap = cv2.VideoCapture(int(self.cam_id))
+        #self.cap = IPWEBCAM(root_url="10.60.60.254:8080")
         self.frame = None
         self.part_id = None
         self.killed = False
@@ -37,6 +64,16 @@ class Camera():
                              value_serializer=lambda value: json.dumps(value).encode(), )
         self.th = None
         self.poll_for_part_id()
+
+
+        if camera_id.find(".") > -1:
+            self.cap = cv2.VideoCapture(int(self.cam_id))
+            self.is_ip = False
+        else:
+            self.cap = IPWEBCAM(root_url=camera_id)
+            self.is_ip = True
+
+
     def _poll_for_part_id(self):
         mp = MongoHelper().getCollection("cam_to_part")
         while True:
@@ -44,14 +81,19 @@ class Camera():
             if pr:
                 #print('found_part : : : ' , pr)
                 self.part_id = pr['part_id']
+
     def poll_for_part_id(self):
         self.th = threading.Thread(target=self._poll_for_part_id)
         self.th.start()
+
     def _start(self):
         while not self.killed:
-            ret, self.frame = self.cap.read()
+            if self.is_ip == False:
+                ret, self.frame = self.cap.read()
+            else:
+                ret, self.frame = self.cap.get_image()
             if self.frame is None:
-                print("None Frame Recieved!") 
+                print("None Frame Recieved!")
                 continue
             #### check tfms and apply here
             if self.part_id:
@@ -59,8 +101,9 @@ class Camera():
                 if self.tfms:
                     print(self.tfms)
                     self.apply_transform()
-            #### 
-            self.publish_frame_to_kafka()           
+            ####
+            self.publish_frame_to_kafka()
+
     def publish_frame_to_kafka(self):
         f_name = str(self.cam_id) + "_tmp.jpg"
         cv2.imwrite(f_name, self.frame)
@@ -69,6 +112,7 @@ class Camera():
         payload_video_frame = {"frame": str(im_b64)}
         future = self.producer.send(self.topic, value=payload_video_frame)
         report = future.get()
+
     def get_tfms(self):
         self.tfms = {}
         if self.part_id:
@@ -80,6 +124,7 @@ class Camera():
                     self.tfms[key] = value
                     #print(self.tfms[key])
         return self.tfms
+
     def apply_transform(self):
         if self.tfms:
             for k,val in self.tfms.items():
@@ -108,48 +153,16 @@ class Camera():
                     else:
                         self.frame = self.frame
 
-
     def start(self):
         self.thread = threading.Thread(target=self._start)
         self.thread.start()
+
     def stop(self):
         self.killed = True
         time.sleep(0.2)
         self.thread.join()
         self.thread = None
         self.killed = False
-
-
-
-"""
-def start_multiprocess_stream(KAFKA_BROKER_URL, topic, camera_id):
-    producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL,
-                             value_serializer=lambda value: json.dumps(value).encode(), )
-
-    cap = cv2.VideoCapture(int(camera_id))
-    print(camera_id)
-
-    while True:
-        ret, img = cap.read()
-        if img is None:
-            print("none frame recieved")
-            
-            #print(camera_id)
-            continue
-        f_name = str(camera_id) + "_tmp.jpg"
-        cv2.imwrite(f_name, img)
-        with open(f_name, 'rb') as f:
-            im_b64 = base64.b64encode(f.read())
-        payload_video_frame = {"frame": str(im_b64)}
-        future = producer.send(topic, value=payload_video_frame)
-        report = future.get()
-        
-        print(report.offset)
-
-    return 0
-
-"""
-
 
 list_of_topics = []
 mp = MongoHelper().getCollection(WORKSTATION_COLLECTION)
@@ -166,18 +179,9 @@ if workstations:
             list_of_topics.append(str(wids)+"_"+str(camera_ids)+"_input")
 
 
-
-
 for i in list_of_topics:
     topic = i
     camera_id = int(topic.split('_')[1])
 
-    #p = multiprocessing.Process(target=start_multiprocess_stream, args=(KAFKA_BROKER_URL, topic,camera_id,))
-    #p.start()
-
     cc = Camera(KAFKA_BROKER_URL, topic, camera_id)
     cc.start()
-
-
-
-#p.join()
--- backend/LIVIS/livis/dashboards/utils.py
+++ backend/LIVIS/livis/dashboards/utils.py
@@ -5,6 +5,7 @@ from livis.settings import *
 from common.utils import MongoHelper
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+import datetime
 
 def total_production_util():
     #from inspection get part id
@@ -19,9 +20,9 @@ def total_production_util():
                 mp = MongoHelper.getCollection(inspection_id+"_log")
             except:
                 continue
-            parts_coll = [p for p in mp.find()]
+            parts_coll = mp.find().count()
             print(parts_coll)
-            total_production_count = total_production_count +len(parts_col)
+            total_production_count = total_production_count + parts_coll
     return total_production_count
 
 def production_yield_util():
@@ -30,7 +31,7 @@ def production_yield_util():
     #iterate through {part_id}_logs
     ### increment count if isAccepted==True
     #Calculate the %of total_production_accepted/total_production
-    total_prod_count = prodcution_yield_util()
+    total_prod_count = total_production_util()
     total_accepted = 0
     mp = MongoHelper().getCollection(INSPECTION_COLLECTION)
     objs = [i for i in mp.find()]
@@ -42,39 +43,78 @@ def production_yield_util():
     percent_yield = (total_accepted/total_prod_count)*100
     return percent_yield
 
+
 def production_rate_util():
-   # from front end get time range for example: hourly, seconds, minutes
-   #from inspection get part id
-   #iterate through part_id logs 
-   ###increment  count of parts for every minute difference
-   #get average according to desired time range hourly, seconds, minutes etc.
-   #return average
-   return 0
+    # get average_time_rate from front end whether secs, min, hours
+    # for every inspection_id log collection
+    ######for every document
+    ######Convert end time to start time to seconds
+    ######add to diff_secs counter
+    ######Increment doc_count for every iter
+    #### average according to time rate (diff_secs_counter)/doc_count
+    time_period = "secs"
+    seconds_count = 0
+    doc_count = 0
+    mp = MongoHelper().getCollection(INSPECTION_COLLECTION)
+    objs = [i for i in mp.find()]
+    for ins in objs:
+        inspection_id = str(ins['_id'])
+        mp = MongoHelper.getCollection(inspection_id + "_log")
+        insp_colls = [p for p in mp.find()]
+        for i in insp_colls:
+            start_time = i["inference_start_time"]
+            end_time = i["inference_end_time"]
+            ### Assuming hours is a 12 hour clock
+            start_time_dt = datetime.datetime.strptime(start_time,"%Y-%m-%d %H:%M:%S")
+            end_time_dt = datetime.datetime.strptime(end_time,"%Y-%m-%d %H:%M:%S")
+            time_delta = end_time_dt - start_time_dt
+            seconds = time_delta.total_seconds()
+            seconds_count = seconds_count + seconds
+        doc_count = doc_count + len(insp_colls)
+    avg_rate_secs = int(seconds_count/doc_count)
+    ## logic needs to be added if time_period is hours or minutes
+    return 0
 
 def defect_count_util():
-   #from inspection get part id
-   ### iterate through {part_id}_logs
-   ### increment count if isAccepted==False
-   # return count
-   total_rejected = 0
-   mp = MongoHelper().getCollection(INSPECTION_COLLECTION)
-   objs = [i for i in mp.find()]
-   for ins in objs:
-       inspection_id = str(ins['_id'])
-       mp = MongoHelper.getCollection(inspection_id+"_log")
-       parts_coll = [p for p in mp.find({"isAccepted":"False"})]
-       total_rejected = total_rejected + len(parts_coll)
-   return total_rejected
+    #from inspection get part id
+    ### iterate through {part_id}_logs
+    ### increment count if isAccepted==False
+    # return count
+    total_rejected = 0
+    mp = MongoHelper().getCollection(INSPECTION_COLLECTION)
+    objs = [i for i in mp.find()]
+    for ins in objs:
+        inspection_id = str(ins['_id'])
+        mp = MongoHelper.getCollection(inspection_id+"_log")
+        parts_coll = [p for p in mp.find({"isAccepted":"False"})]
+        total_rejected = total_rejected + len(parts_coll)
+    return total_rejected
 
 def total_vs_planned_util():
-   #call "total_production_util" for total_production count
-   #access plan collection
-   ###increment count of planned_production_count
-   # return (total_production_count, planned_production_count)
-   total_production_count = 0
-   mp = MongoHelper().getCollection(PLAN_COLLECTION)
-   plan_colls= [p for p in mp.find()]
-   total_planned_count = 0
-   for plan in plan_colls:
-       total_planned_count = total_planned_count + int(plan["planned_production_count"])
-   return total_planned_count
+    #call "total_production_util" for total_production count
+    #access plan collection
+    ###increment count of planned_production_count
+    # return (total_production_count, planned_production_count)
+    total_production_count = total_production_util()
+    mp = MongoHelper().getCollection(PLAN_COLLECTION)
+    plan_colls= [p for p in mp.find()]
+    total_planned_count = 0
+    for plan in plan_colls:
+        total_planned_count = total_planned_count + int(plan["planned_production_count"])
+    return total_planned_count
+
+def defect_distribution():
+    defect_count = {}
+    mp = MongoHelper().getCollection(INSPECTION_COLLECTION)
+    pr = [i for i in mp.find()]
+    for p in pr:
+        inspection_id = str(p['_id'])
+        mp = MongoHelper.getCollection(inspection_id + "_log")
+        parts_coll = [p for p in mp.find({"isAccepted": "False"})]
+        for i in parts_coll:
+            for j in i['detections_missed']:
+                if j in defect_count:
+                    defect_count[j] +=1
+                else:
+                    defect_count[j] = 1
+    return defect_count
