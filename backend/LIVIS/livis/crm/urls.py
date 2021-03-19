282eae723810d98898dbeb7e5dc8adfb6391e76c
Lincode <lincode@pop-os.localdomain>
2021-03-19 18:10:02 +0530

    crm lead cruds2

--- backend/LIVIS/livis/crm/urls.py
+++ backend/LIVIS/livis/crm/urls.py
@@ -3,5 +3,10 @@ from crm import views
 
 
 urlpatterns = [
-    # re_path(r'^testLogs/$', views.test_logs),
+    re_path(r'^update_lead/$', views.update_lead),
+    re_path(r'^get_all_leads/$', views.get_all_leads),
+    re_path(r'^get_single_lead/(?P<id>[A-Za-z0-9-_]+)/$', views.get_single_lead),
+    re_path(r'^create_lead/$', views.create_lead),
+    re_path(r'^delete_lead/(?P<id>[A-Za-z0-9-_]+)/$', views.delete_lead),
+    re_path(r'^check_gst/(?P<gst>[A-Za-z0-9-_]+)/$', views.check_gst)
 ]
--- backend/LIVIS/livis/crm/views.py
+++ backend/LIVIS/livis/crm/views.py
@@ -6,76 +6,67 @@ from django.http import HttpResponse
 from crm.utils import *
 import json
 from common.utils import *
-
 from logs.utils import add_logs_util
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-'''
+@api_view(['PATCH'])
+@renderer_classes((TemplateHTMLRenderer,))
+@csrf_exempt
+@permission_classes((AllowAny,))
+def update_lead(request):
+    data = json.loads(request.body)
+    message, status_code = update_lead_util(data)
+    if status_code == 200:
+        return HttpResponse(json.dumps({'Message': 'Success!', 'data': message}), content_type="application/json")
+    else:
+        return HttpResponse(json.dumps({'Message': 'fail!', 'data': message}), content_type="application/json")
+
+
+@api_view(['DELETE'])
+@renderer_classes((TemplateHTMLRenderer,))
+@csrf_exempt
+@permission_classes((AllowAny,))
+def delete_lead(request):
+    data = json.loads(request.body)
+    message, status_code = delete_lead_util(id)
+    if status_code == 200:
+        return HttpResponse(json.dumps({'Message': 'Success!', 'data': message}), content_type="application/json")
+    else:
+        return HttpResponse(json.dumps({'Message': 'fail!', 'data': message}), content_type="application/json")
 
 @api_view(['POST'])
-@renderer_classes((TemplateHTMLRenderer,JSONRenderer))
+@renderer_classes((TemplateHTMLRenderer,))
 @csrf_exempt
-def add_assects(request):
-    token_user_id = request.user.user_id
-    operation_type = "asset"
-    notes = "add assets"
-    
-    add_logs_util(token_user_id,operation_type,notes)
+@permission_classes((AllowAny,))
+def create_lead(request):
     data = json.loads(request.body)
-    response,status_code = add_assects_util(data)
-    if status_code != 200:
-        return HttpResponse( {message}, status=status_code)
+    message, status_code = create_lead_util(data)
+    if status_code == 200:
+        return HttpResponse(json.dumps({'Message': 'Success!', 'data': message}), content_type="application/json")
     else:
-        return HttpResponse(json.dumps(response, cls=Encoder), content_type="application/json")
+        return HttpResponse(json.dumps({'Message': 'fail!', 'data': message}), content_type="application/json")
 
-        
-
-@api_view(['POST'])
-@renderer_classes((TemplateHTMLRenderer,JSONRenderer))
+@api_view(['GET'])
 @csrf_exempt
-def update_assect(request):
-    token_user_id = request.user.user_id
-    operation_type = "asset"
-    notes = "update asset"
-    
-    add_logs_util(token_user_id,operation_type,notes)
-    data = json.loads(request.body)
-    response,status_code = update_assect_util(data)
-    if status_code != 200:
-        return HttpResponse( {message}, status=status_code)
-    else:
-        return HttpResponse(json.dumps(response, cls=Encoder), content_type="application/json")
+@permission_classes((AllowAny,))
+def get_all_leads(request):
+    leads_list = get_all_leads_util()
+    return HttpResponse(json.dumps( leads_list , cls=Encoder), content_type="application/json")
 
 
 @api_view(['GET'])
-@renderer_classes((TemplateHTMLRenderer,JSONRenderer))
 @csrf_exempt
 @permission_classes((AllowAny,))
-def get_assect(request):
-    #token_user_id = request.user.user_id
-    #operation_type = "asset"
-    #notes = "get asset"
-    
-    #add_logs_util(token_user_id,operation_type,notes)
-    #data = json.loads(request.body)
-    
-    
-    response,status_code = get_assect_util()
-    if status_code != 200:
-        return HttpResponse( {message}, status=status_code)
-    else:
-        return HttpResponse(json.dumps(response, cls=Encoder), content_type="application/json")
+def get_single_lead(request, id):
+    leads_list = get_single_lead_util(id)
+    return HttpResponse(json.dumps(leads_list , cls=Encoder), content_type="application/json")
 
 @api_view(['GET'])
-@renderer_classes((TemplateHTMLRenderer,JSONRenderer))
 @csrf_exempt
 @permission_classes((AllowAny,))
-def validate(request):
-    response,status_code = get_validation()
-    if status_code != 200:
-        return HttpResponse( {message}, status=status_code)
-    else:
-        return HttpResponse(json.dumps(response, cls=Encoder), content_type="application/json")
-'''
+def check_gst(request, gst):
+    response = check_gst_util(gst)
+    return HttpResponse(json.dumps(response, cls=Encoder), content_type="application/json")
+
+
