e768cba176cb85f94ccd2e15dad6b7e498e5ddb5
Ubuntu <ubuntu@ip-172-31-43-79.ap-south-1.compute.internal>
2021-04-05 12:25:50 +0000

    branch change

--- backend/LIVIS/livis/training/urls.py
+++ backend/LIVIS/livis/training/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
                url(r'^create_retrain_experiment/$', views.create_retrain_experiment),
                url(r'^get_model_data/(?P<experiment_type>[A-Za-z0-9-_]+)$', views.get_model_data),
               # url(r'^get_deployment_list/$', views.get_deployment_list),
-               url(r'^get_deployment_list/$', views.get_deployment_list_updated),
-               url(r'^get_all_running_experiments/$', views.get_all_running_experiments), 
+               # url(r'^get_deployment_list/$', views.get_deployment_list_updated),
+               url(r'^get_deployment_list/$', views.deployment_list_filter),
+            #    url(r'^get_all_running_experiments/$', views.get_all_running_experiments),
+               url(r'^get_all_running_experiments/$', views.all_experiments_filter), 
                #url(r'^get_running_experiment/(?P<part_id>[A-Za-z0-9-_]+)$', views.get_running_experiment), 
                url(r'^experiment_status/(?P<part_id>[A-Za-z0-9-_]+)/(?P<experiment_id>[A-Za-z0-9-_]+)$', views.get_experiment_status),
                
